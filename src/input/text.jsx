f727d951074208a9663aa73eb4f9e68cf14fb8bf
Barry Shao <bshao@envisio.com>
2019-04-08 10:13:47 -0700

    ✨

--- src/input/text.jsx
+++ src/input/text.jsx
@@ -1,87 +1,147 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from 'react';
 import { Mutation } from 'react-apollo';
+import PropTypes from 'prop-types';
 import {
   capitalize,
   set,
   size,
   gt,
+  debounce,
 } from 'lodash';
 
-export default function Input({
-  mutation,
-  variables,
-  valuePath,
-  variablesFormatter,
-  mutateEvent,
-  type,
-  pattern,
-  ...props
-}) {
-  return (
-    <Mutation mutation={mutation}>
-      {(mutate) => {
-        const eventProps = {
-          [`on${capitalize(mutateEvent)}`]: ({
-            target: { value: newValue },
-          }) => {
-            if (pattern && gt(size(pattern), 0)) {
-              const regexp = new RegExp(pattern);
+export default class Input extends Component {
+  static propTypes = {
+    mutation: PropTypes.object.isRequired,
+    variables: PropTypes.object,
+    update: PropTypes.func,
+    valuePath: PropTypes.string,
+    variablesFormatter: PropTypes.func,
+    mutateEvent: PropTypes.string.isRequired,
+    type: PropTypes.string,
+    pattern: PropTypes.string,
+    debouncable: PropTypes.bool,
+    wait: PropTypes.number,
+    defaultValue: PropTypes.oneOfType([
+      PropTypes.string,
+      PropTypes.number,
+      PropTypes.bool,
+      PropTypes.object,
+      PropTypes.undefined,
+    ]),
+  }
 
-              if (newValue.match(regexp)) {
-                mutate({
-                  variables: variablesFormatter({
-                    variables,
-                    valuePath,
-                    value: newValue,
-                  }),
-                });
+  static defaultProps = {
+    variables: {
+      variables: {},
+    },
+    variablesFormatter: ({
+      variables,
+      valuePath,
+      value,
+    }) => set(variables, valuePath, value),
+    valuePath: '',
+    update: null,
+    type: undefined,
+    pattern: undefined,
+    debouncable: false,
+    wait: 0,
+    defaultValue: '',
+  }
+
+  constructor(props) {
+    super(props);
+
+    const { defaultValue } = this.props;
+
+    this.state = { value: defaultValue };
+  }
+
+  updateHandler = (mutate, value) => {
+    const {
+      variablesFormatter,
+      variables,
+      valuePath,
+    } = this.props;
+
+    mutate({
+      variables: variablesFormatter({
+        variables,
+        valuePath,
+        value,
+      }),
+    });
+  }
+
+  debounceUpdateHandler = debounce((mutate) => {
+    const {
+      variablesFormatter,
+      variables,
+      valuePath,
+    } = this.props;
+    const { value } = this.state;
+
+    mutate({
+      variables: variablesFormatter({
+        variables,
+        valuePath,
+        value,
+      }),
+    });
+  }, this.props.wait)
+
+  render() {
+    const {
+      mutation,
+      variables,
+      valuePath,
+      variablesFormatter,
+      mutateEvent,
+      type,
+      pattern,
+      debouncable,
+      wait,
+      ...props
+    } = this.props;
+
+    return (
+      <Mutation mutation={mutation}>
+        {(mutate) => {
+          const eventProps = {
+            [`on${capitalize(mutateEvent)}`]: ({
+              target: { value },
+            }) => {
+              if (pattern && gt(size(pattern), 0)) {
+                const regexp = new RegExp(pattern);
+
+                if (value.match(regexp)) {
+                  if (debouncable) {
+                    this.setState({ value });
+                    this.debounceUpdateHandler(mutate);
+                  } else {
+                    this.updateHandler(mutate, value);
+                  }
+                } else {
+                  // onInvalid
+                }
+              } else {
+                if (debouncable) {
+                  this.setState({ value });
+                  this.debounceUpdateHandler(mutate);
+                } else {
+                  this.updateHandler(mutate, value);
+                }
               }
-            } else {
-              mutate({
-                variables: variablesFormatter({
-                  variables,
-                  valuePath,
-                  value: newValue,
-                }),
-              });
-            }
-          },
-        };
+            },
+          };
 
-        return (
-          <input
-            {...props}
-            {...eventProps}
-          />
-        );
-      }}
-    </Mutation>
-  );
+          return (
+            <input
+              {...props}
+              {...eventProps}
+            />
+          );
+        }}
+      </Mutation>
+    );
+  }
 }
-
-Input.propTypes = {
-  mutation: PropTypes.object.isRequired,
-  variables: PropTypes.object,
-  update: PropTypes.func,
-  valuePath: PropTypes.string,
-  variablesFormatter: PropTypes.func,
-  mutateEvent: PropTypes.string.isRequired,
-  type: PropTypes.string,
-  pattern: PropTypes.string,
-};
-
-Input.defaultProps = {
-  variables: {
-    variables: {},
-  },
-  variablesFormatter: ({
-    variables,
-    valuePath,
-    value,
-  }) => set(variables, valuePath, value),
-  valuePath: '',
-  update: null,
-  type: undefined,
-  pattern: undefined,
-};
